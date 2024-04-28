f0f8812b95c43323a7599979e05fba49adb00c8a
gabrielcaiana <gabrielcaianaguedes@gmail.com>
2024-04-28 17:48:24 -0300

    refactor: adds improvements reported by sonarqube

--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ export const fib = () => {
   const fibInner = function (n) {
       if (n in memo) return memo[n];
       if (n <= 1) return n;
-      return memo[n] = fibInner(n - 1) + fibInner(n - 2);
+      const result = fibInner(n - 1) + fibInner(n - 2); 
+      return memo[n] = result;
   };
   return fibInner;
 };
